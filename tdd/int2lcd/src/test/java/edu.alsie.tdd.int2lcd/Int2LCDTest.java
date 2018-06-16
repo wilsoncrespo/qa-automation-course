f0e0548f9d909c7db46e16950e8fbc15105d72d5
wilson crespo <wilsoncrespo@gmail.com>
2018-06-16 04:09:13 -0400

    INT2LCD-TDD: second failing test added

--- tdd/int2lcd/src/test/java/edu.alsie.tdd.int2lcd/Int2LCDTest.java
+++ tdd/int2lcd/src/test/java/edu.alsie.tdd.int2lcd/Int2LCDTest.java
@@ -16,4 +16,23 @@ public class Int2LCDTest {
 
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testLCDCellIsZero()
+    {
+        Int2LCD int2LCD = new Int2LCD();
+        int[] zeroLCDCell = int2LCD.getEmptyLCDCell();
+        /**
+         *
+         *  _             1
+         * | |    =>    1 0 1    =>    1101111
+         * |_|          1 1 1
+         *
+         */
+        int[] expected = new int[]{1,1,0,1,1,1,1};
+
+        int[] actual = zeroLCDCell;
+
+        Assert.assertArrayEquals(expected, actual);
+    }
 }
