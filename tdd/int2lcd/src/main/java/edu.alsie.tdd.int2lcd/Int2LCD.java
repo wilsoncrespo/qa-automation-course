224d0aa53fa4d3a89ac065e7e98301d9e7898bbb
wilson crespo <wilsoncrespo@gmail.com>
2018-06-16 20:15:48 -0400

    INT2LCD-TDD: implementation added for retrieving the string value of the cell for digit zero

--- tdd/int2lcd/src/main/java/edu.alsie.tdd.int2lcd/Int2LCD.java
+++ tdd/int2lcd/src/main/java/edu.alsie.tdd.int2lcd/Int2LCD.java
@@ -35,4 +35,37 @@ public class Int2LCD {
     public int[] getNineLCDCell() {
         return new int[]{1,1,1,1,0,1,1};
     }
+
+    public String[] getZeroLCDCellStringValue()
+    {
+        int[] zeroLCDCellValue = getZeroLCDCell();
+        return convertValuesToString(zeroLCDCellValue);
+    }
+
+    private String[] convertValuesToString(int[] lcdCellValue)
+    {
+        String[] stringCellValue = new String[3];
+        for (int i = 0; i < lcdCellValue.length; i++) {
+            if (i == 0)
+            {
+                stringCellValue[0] = getStringRow(0, lcdCellValue[i], 0);
+            }
+            if (i == 1)
+            {
+                stringCellValue[1] = getStringRow(lcdCellValue[i], lcdCellValue[i+1], lcdCellValue[i+2]);
+                i += 2;
+            }
+            if (i == 4)
+            {
+                stringCellValue[2] = getStringRow(lcdCellValue[i], lcdCellValue[i+1], lcdCellValue[i+2]);
+                i += 2;
+            }
+        }
+        return stringCellValue;
+    }
+
+    private String getStringRow(int left, int central, int right)
+    {
+        return (left == 1 ? "|" : " ") + (central == 1 ? "_" : " ") + (right == 1 ? "|" : " ");
+    }
 }
